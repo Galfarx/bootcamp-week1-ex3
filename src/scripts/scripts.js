81f829c5fc7d9c83e2bc0e7efdc717479bf14f6a
Dominik Kubis <kubis.dominik1@gmail.com>
2016-11-08 22:36:01 +0100

    [TASK-2] - add fancy-sidebar

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,7 +1,9 @@
 (function ($) {
     var $hamburger = $('.fancy-hamburger');
+    var $fancySidebarContainer = $('.js_fancy_sidebar');
 
     $hamburger.on('click', function () {
         $(this).toggleClass('open');
+        $fancySidebarContainer.toggleClass('open');
     })
 })(jQuery);
